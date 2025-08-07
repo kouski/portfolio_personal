ed0e0ae08c73c24378d023a077c67a9927ff2c39
kouski <72550619+kouski@users.noreply.github.com>
2025-08-07 11:45:48 +0200

    Cambios subida proyecto paqueteria

--- data.js
+++ data.js
@@ -115,7 +115,7 @@ const proyectos = [
     {
       titulo: "Creación Agente IA para consultar envíos de paquetes",
       descripcion: "Creación de un flujo de automatización que recibe mediante un chat peticiones de clientes para saber el estado de sus envíos, usando N8N,Ollama,google sheets...",
-      imagen: "/images/flujo_envios.png",
+      imagen: "/images/Flujo_envios.png",
       enlace: "/Doc_agenteia_paqueteria.pdf",
       categoria: "IA",
     },
